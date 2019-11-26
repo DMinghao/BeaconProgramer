6236c6231d98f979b1d68dc8b20ed9808c3a7b6d
DMinghao <45401748+DMinghao@users.noreply.github.com>
2019-11-26 16:52:27 -0600

    Clean up and prepare for packaging

---
+++ src/main/java/museumapp/beaconprogramer/Command.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package museumapp.beaconprogramer;
+
+import Entity.Bluetooth;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import jssc.SerialPortList;
+
+/**
+ *
+ * @author duter
+ */
+public class Command {
+
+    private static Process p;
+    private static Thread cmdErr;
+    private static Thread cmdOut;
+    private static PrintWriter stdin;
+
+    private final static String MAIN_PATH = System.getProperty("user.dir");
+    private final static String BEACON_C_PATH = MAIN_PATH + "\\Beacon\\Workspace\\Projects\\ble_app_blinky\\main.c";
+    private final static String MAKEFILE = MAIN_PATH + "\\Beacon\\Workspace\\Projects\\ble_app_blinky\\pca10059\\s140\\armgcc";
+
+    private static PrintWriter pw() throws IOException {
+        try {
+            p = Runtime.getRuntime().exec("cmd");
+            cmdErr = new Thread(new SyncPipe(p.getErrorStream(), System.err));
+            cmdOut = new Thread(new SyncPipe(p.getInputStream(), System.out));
+            cmdErr.start();
+            cmdOut.start();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return new PrintWriter(p.getOutputStream());
+    }
+
+    private static void RunCommand(ArrayList<String> input) {
+        try {
+            stdin = pw();
+            for (String i : input) {
+                stdin.println(i);
+            }
+            stdin.close();
+            p.waitFor();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static String getBeaconPort() {
+        String[] portNames = SerialPortList.getPortNames();
+
+        if (portNames.length == 0 || portNames.length > 1) {
+            if (portNames.length == 0) {
+                System.out.println("/nBeacon not found...");
+            }
+            if (portNames.length > 1) {
+                System.out.println("/nMultiple devices detected, you can only program one beacon at a time...");
+            }
+            System.out.println("Press Enter to exit...");
+            try {
+                System.in.read();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.exit(-1);
+        }
+        System.out.println("Beacon Port Name : " + portNames[0]);
+        return portNames[0];
+
+    }
+
+    public static void CompileBeaconC(Bluetooth beacon) {
+        NameBeacon(beacon);
+
+        ArrayList<String> cmd = new ArrayList<String>();
+
+        cmd.add("cd " + MAKEFILE);
+        cmd.add("rmdir /q /s _build");
+        cmd.add("make");
+        cmd.add("xcopy s140_nrf52_7.0.1_softdevice.hex _build");
+
+        RunCommand(cmd);
+        ResetBeaconC(beacon);
+    }
+
+    private static void NameBeacon(Bluetooth beacon) {
+        try {
+            Path path = Paths.get(BEACON_C_PATH);
+            Charset cset = StandardCharsets.UTF_8;
+            String content = new String(Files.readAllBytes(path), cset);
+            content = content.replaceAll("#define DEVICE_NAME \"\" ", "#define DEVICE_NAME \"" + beacon.getName() + "\" ");
+            Files.write(path, content.getBytes(cset));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void ResetBeaconC(Bluetooth beacon) {
+        try {
+            Path path = Paths.get(BEACON_C_PATH);
+            Charset cset = StandardCharsets.UTF_8;
+            String content = new String(Files.readAllBytes(path), cset);
+            content = content.replaceAll("#define DEVICE_NAME \"" + beacon.getName() + "\" ", "#define DEVICE_NAME \"\" ");
+            Files.write(path, content.getBytes(cset));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void ProgramBeacon(String BeaconPort) {
+        ArrayList<String> cmd = new ArrayList<String>();
+        cmd.add("cd " + MAKEFILE + "\\_build");
+        cmd.add("powershell");
+        cmd.add("nrfutil pkg generate --hw-version 52 --debug-mode --sd-req 0xCA --sd-id 0xCA --application .\\nrf52840_xxaa.hex --softdevice .\\s140_nrf52_7.0.1_softdevice.hex beacon.zip");
+        cmd.add("nrfutil dfu usb-serial -pkg beacon.zip -p " + BeaconPort + " -b 115200");
+        RunCommand(cmd);
+    }
+
+}
--- src/main/java/museumapp/beaconprogramer/Database.java
+++ src/main/java/museumapp/beaconprogramer/Database.java
@@ -5,29 +5,47 @@
  */
 package museumapp.beaconprogramer;
 
+import Entity.Bluetooth;
+import Entity.Exhibit;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
  * @author mdu18
  */
 public class Database {
-    
-    public Database(){
+
+    private static ArrayList<Exhibit> EList;
+
+    private static ArrayList<Bluetooth> BList;
+
+    private static Connection conn;
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public Database() {
+        this.conn = connect();
+        if (!ReadInData()) {
+            System.out.println("Failed to read from database");
+            System.exit(-1);
+        }
     }
-    
-    public Connection connect(){
+
+    private static Connection connect() {
         // Connect to database
         String hostName = "alabamamuseum.database.windows.net"; // update me
         String dbName = "MuseumApp"; // update me
         String user = "museumadmin"; // update me
         String password = "Alabama123"; // update me
         String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
-            + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+                + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
         Connection connection = null;
 
         try {
@@ -35,11 +53,96 @@ public class Database {
             String schema = connection.getSchema();
             System.out.println("Successful connection - Schema: " + schema);
 
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        
-        return connection; 
+
+        return connection;
     }
+
+    private static boolean ReadInData() {
+        String selectSql = "SELECT * FROM EXHIBIT ";
+
+        try (Statement statement = conn.createStatement(); ResultSet resultSet = statement.executeQuery(selectSql)) {
+
+            while (resultSet.next()) {
+                EList.add(new Exhibit(resultSet.getInt(1), resultSet.getString(2), resultSet.getInt(3)));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        selectSql = "SELECT * FROM BLUETOOTH";
+
+        try (Statement statement = conn.createStatement(); ResultSet resultSet = statement.executeQuery(selectSql)) {
+
+            while (resultSet.next()) {
+                BList.add(new Bluetooth(resultSet.getInt(1), resultSet.getString(2), resultSet.getInt(3)));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("All on record exhibits: ");
+        for (Exhibit e : EList) {
+            System.out.println(e.toString() + "\n");
+        }
+
+        return (EList != null);
+    }
+
+    public static Bluetooth NewBeacon() {
+        Exhibit selected;
+        String confirm = "";
+        do {
+            do {
+                System.out.println("Please enter an Exhibit ID to pair your new bluetooth beacon ");
+                int SelectedExhibit = Integer.parseInt(sc.nextLine());
+                selected = EList.stream().filter(x -> SelectedExhibit == x.getID()).findFirst().orElse(null);
+            } while (selected == null);
+            System.out.println("Adding a beacon to exhibit " + selected.getID() + " , please confirm. ----- YES/NO");
+            confirm = sc.nextLine();
+        } while (confirm.equalsIgnoreCase("no"));
+
+        Bluetooth b;
+        confirm = "";
+        do {
+            do {
+                System.out.println("Please enter the new bluetooth beacon name");
+                String beaconName = sc.nextLine();
+                b = BList.stream().filter(x -> beaconName == x.getName()).findFirst().orElse(null);
+                if (b != null) {
+                    System.out.println("The name you entered is taken");
+                } else {
+                    int newBID = BList.size() + 1;
+                    b = new Bluetooth(newBID, beaconName, selected.getID());
+                    break;
+                }
+            } while (b != null);
+            System.out.println("Beacon is named with " + b.getName() + " , please confirm. ----- YES/NO");
+            confirm = sc.nextLine();
+        } while (confirm.equalsIgnoreCase("no"));
+
+        return b;
+    }
+
+    public static void InsertBluetooth(Bluetooth b) {
+        System.out.println("Do you want to insert the beacon to the database? (Yes / No)");
+
+        if (sc.nextLine().equalsIgnoreCase("yes")) {
+            String sql = "INSERT INTO BLUETOOTH (BT_ID, BT_NAME, EX_ID)"
+                    + "VALUES ("
+                    + b.getID() + ", '"
+                    + b.getName() + "', "
+                    + b.getExID()
+                    + ");";
+            try {
+                Statement statement = conn.createStatement();
+                statement.executeUpdate(sql);
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+
 }
